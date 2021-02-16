3fc21585cacc36ba3a33b9960b2dbfc629fb385b
kostadinM29 <68953020+kostadinM29@users.noreply.github.com>
2021-02-16 13:11:00 +0200

    Added Sql Injection demo

--- 01. ADO.NET Lab/CSharpEFAdo.netDemo/StartUp.cs
+++ 01. ADO.NET Lab/CSharpEFAdo.netDemo/StartUp.cs
@@ -8,14 +8,36 @@ namespace CSharpEFAdo.netDemo
     {
         static void Main(string[] args)
         {
-            SqlConnection dbConnection =
-                new SqlConnection(@"Server=.\SQLEXPRESS; Database=SoftUni; Integrated Security=true");
+            // sql injection test
+
+            Console.Write("Enter email: ");
+            var email = Console.ReadLine();
+            Console.Write("Enter birth date: ");
+            var birthDate = Console.ReadLine();
+
+
+            var dbcon = new SqlConnection(@"Server=.\SQLEXPRESS; Database=TripService; Integrated Security=true");
+            using (dbcon)
+            {
+                dbcon.Open();
+                var sqlCommand =
+                    new SqlCommand($"SELECT COUNT(*) FROM Accounts WHERE Email = @Email AND BirthDate = @BirthDate;",dbcon);
+                sqlCommand.Parameters.Add(new SqlParameter("@Email", email));
+                sqlCommand.Parameters.Add(new SqlParameter("@BirthDate", birthDate));
+                var usersCount = (int) sqlCommand.ExecuteScalar();
+               string message =  usersCount > 0 ? "Access granted!" : "Access denied!";
+               Console.WriteLine(message);
+            }
+
+
+            // ado.net test
+            SqlConnection dbConnection = new SqlConnection(@"Server=.\SQLEXPRESS; Database=SoftUni; Integrated Security=true");
             dbConnection.Open();
 
             using (dbConnection)
             {
-                SqlCommand command =
-                    new SqlCommand("SELECT FirstName, LastName, d.Name FROM Employees e" + "JOIN Departments d ON e.DepartmentId = d.DepartmentId", dbConnection);
+                SqlCommand command = new SqlCommand("SELECT FirstName, LastName, d.Name FROM Employees e" +
+                                                    "JOIN Departments d ON e.DepartmentId = d.DepartmentId", dbConnection);
                 SqlDataReader reader = command.ExecuteReader();
 
                 using (reader)
