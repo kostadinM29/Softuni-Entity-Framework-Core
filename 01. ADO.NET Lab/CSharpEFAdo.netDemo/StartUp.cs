249dfa1e048fbcef61114c96d3622b8040a1ba0e
kostadinM29 <68953020+kostadinM29@users.noreply.github.com>
2021-02-16 12:05:52 +0200

    Added ADO.NET Lab

---
+++ 01. ADO.NET Lab/CSharpEFAdo.netDemo/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace CSharpEFAdo.netDemo
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection dbConnection =
+                new SqlConnection(@"Server=.\SQLEXPRESS; Database=SoftUni; Integrated Security=true");
+            dbConnection.Open();
+
+            using (dbConnection)
+            {
+                SqlCommand command =
+                    new SqlCommand("SELECT FirstName, LastName, d.Name FROM Employees e" + "JOIN Departments d ON e.DepartmentId = d.DepartmentId", dbConnection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        string firstName = (string)reader["FirstName"];
+                        string lastName = (string)reader["LastName"];
+                        string departmentName = (string)reader["Name"];
+                        Console.WriteLine($"{firstName} {lastName} - {departmentName}");
+                    }
+
+                }
+            }
+        }
+    }
+}
