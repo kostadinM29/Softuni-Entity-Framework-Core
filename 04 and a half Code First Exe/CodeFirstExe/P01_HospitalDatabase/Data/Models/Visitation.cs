fdc60e3b89303df73f615c1f0b87000c23327a30
Kostadin Minov <minovkostadin@gmail.com>
2021-03-03 14:55:52 +0200

    Update Visitation.cs

--- 04 and a half Code First Exe/CodeFirstExe/P01_HospitalDatabase/Data/Models/Visitation.cs
+++ 04 and a half Code First Exe/CodeFirstExe/P01_HospitalDatabase/Data/Models/Visitation.cs
@@ -26,6 +26,6 @@ namespace P01_HospitalDatabase.Data.Models
 
         [ForeignKey("Doctor")]
         public int DoctorId { get; set; }
-        public Doctor Doctor { get; set; }
+        public virtual Doctor Doctor { get; set; }
     }
 }
