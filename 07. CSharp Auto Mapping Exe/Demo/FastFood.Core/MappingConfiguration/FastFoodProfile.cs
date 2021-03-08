7312e3cafdfa0cf2f4e129825a0cc0c8ab33f860
kostadinM29 <68953020+kostadinM29@users.noreply.github.com>
2021-03-08 23:12:53 +0200

    Added Auto Mapping Lab and Exe

---
+++ 07. CSharp Auto Mapping Exe/Demo/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Globalization;
+using FastFood.Core.ViewModels.Categories;
+using FastFood.Core.ViewModels.Employees;
+using FastFood.Core.ViewModels.Items;
+using FastFood.Core.ViewModels.Orders;
+using FastFood.Models.Enums;
+
+namespace FastFood.Core.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Models;
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            //Categories
+            this.CreateMap<Category, CategoryAllViewModel>(); // checks name?
+
+            this.CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.CategoryName)); // category name
+
+            //Employees
+            this.CreateMap<Employee, EmployeesAllViewModel>()
+                .ForMember(x => x.Position, y => y.MapFrom(s => s.Position.Name)); // only position name needed
+
+            this.CreateMap<RegisterEmployeeInputModel, Employee>();
+
+            this.CreateMap<Position, RegisterEmployeeViewModel>()
+                .ForMember(x => x.PositionId, y => y.MapFrom(s => s.Id));
+
+            //Items
+            this.CreateMap<CreateItemInputModel, Item>();
+
+            this.CreateMap<Category, CreateItemViewModel>()
+                .ForMember(x => x.CategoryId, y => y.MapFrom(s => s.Id));
+
+            this.CreateMap<Item, ItemsAllViewModels>()
+                .ForMember(x => x.Category, y => y.MapFrom(s => s.Category.Name)); // get name
+
+            //Orders
+
+            this.CreateMap<Item, CreateOrderItemViewModel>() // have to create new view model
+               .ForMember(x => x.ItemId, y => y.MapFrom(s => s.Id))
+               .ForMember(x => x.ItemName, y => y.MapFrom(s => s.Name));
+
+            this.CreateMap<Employee, CreateOrderEmployeeViewModel>() // have to create new view model
+                .ForMember(x => x.EmployeeId, y => y.MapFrom(s => s.Id))
+                .ForMember(x => x.EmployeeName, y => y.MapFrom(s => s.Name));
+
+            this.CreateMap<CreateOrderInputModel, Order>() // make new order
+                .ForMember(x => x.DateTime, y => y.MapFrom(s => DateTime.UtcNow))
+                .ForMember(x => x.Type, y => y.MapFrom(s => OrderType.ToGo));
+
+            this.CreateMap<CreateOrderInputModel, OrderItem>() // and link it with items
+                .ForMember(x => x.ItemId, y => y.MapFrom(s => s.ItemId))
+                .ForMember(x => x.Quantity, y => y.MapFrom(s => s.Quantity));
+
+            this.CreateMap<Order, OrderAllViewModel>()
+                .ForMember(x => x.Employee, y => y.MapFrom(s => s.Employee.Name))
+                .ForMember(x => x.DateTime, y => y.MapFrom(s => s.DateTime.ToString("D", CultureInfo.InvariantCulture)))
+                .ForMember(x => x.OrderId, y => y.MapFrom(s => s.Id));
+
+
+
+
+        }
+    }
+}
